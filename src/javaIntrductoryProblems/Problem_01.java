122fa9445c58f794901d00862d7b3683d8b395e6
Md. Tanvir Sadik Raj <32238769+TSraj@users.noreply.github.com>
2022-06-11 02:21:14 +0600

    problem01

--- src/javaIntrductoryProblems/Problem_01.java
+++ src/javaIntrductoryProblems/Problem_01.java
@@ -1,5 +1,16 @@
 package javaIntrductoryProblems;
 
-public class Problem_01 {
+import java.util.Scanner;
 
+public class Problem_01 {
+	 public static void main(String[] args) {
+	        Scanner scan = new Scanner(System.in);
+	        int a = scan.nextInt();
+	        int b = scan.nextInt();
+	        int c = scan.nextInt();
+
+	        System.out.println(a);
+	        System.out.println(b);
+	        System.out.println(c);
+	    }
 }
