4abd3ad1cfa30c458ff6bcd11a748af94b7dabb9
Md. Tanvir Sadik Raj <32238769+TSraj@users.noreply.github.com>
2022-06-12 01:33:07 +0600

    probelm3 vhange & problem 4

--- src/javaIntrductoryProblems/Problem_03.java
+++ src/javaIntrductoryProblems/Problem_03.java
@@ -1,5 +1,18 @@
 package javaIntrductoryProblems;
 
-public class Problem_03 {
+import java.util.Scanner;
 
+public class Problem_03 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        scan.close();
+        
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+	}
 }
